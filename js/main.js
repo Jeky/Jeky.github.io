d1bb9809fd35c4a4b2269eb695c94cf09f18fe19
Xiutian Cui <cxiutian@amazon.com>
2021-02-08 11:07:13 -0500

    Refined website structure

--- js/main.js
+++ js/main.js
@@ -5,15 +5,33 @@ function render(file, element_id) {
                 try {
                     return '<pre class="hljs"><code>' +
                         hljs.highlight(lang, str, true).value +
-                        '</code></pre>';
+                        '</code></pre>'
                 } catch (__) { }
             }
 
-            return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+            return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>'
         }
     })
 
-    fetch(`articles/${file}`)
+    fetch(file)
         .then(response => response.text())
         .then(text => document.getElementById(element_id).innerHTML = md.render(text))
+}
+
+function parse_directory(node) {
+    return [
+        node.root ? '<h1>' : '<ul><li>',
+        node.link ? `<a href="${node.link}">` : '',
+        node.name,
+        node.root ? '</h1><ul>': '',
+        node.children.map(parse_directory).join(''),
+        node.link ? '</a>' : '',
+        node.root ? '' : '</ul>'
+    ].join('')
+}
+
+function fill_directory(directory_json, element_id) {
+    fetch(directory_json)
+        .then(response => response.json())
+        .then(directory => document.getElementById(element_id).innerHTML = parse_directory(directory))
 }
